531e236848606f203b3f95d1a0b75d447be11e98
Muhamet Osmani <muhamet.osmani@it-labs.com>
2022-03-26 13:14:19 +0100

    InititalCommit

---
+++ Erasmus.Domain/DTO/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Erasmus.Domain.DTO
+{
+    public class TestClass
+    {
+        public string Name { get; set; }
+    }
+}
